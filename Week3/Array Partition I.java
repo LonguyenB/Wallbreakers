3c4e21a7b9d9a93653c5c9c0f7e480b100cc7b75
LonguyenB <longnguyen1993@gmail.com>
2019-07-15 16:25:50 -0400

    Update Array Partition I.java

--- Week3/Array Partition I.java
+++ Week3/Array Partition I.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        // sort, then add the min of every two numbers
+        Arrays.sort(nums);
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i = i + 2) {
+            sum = sum + Math.min(nums[i], nums[i + 1]);
+        }
+        
+        return sum;
+    }
+}
